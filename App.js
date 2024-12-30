f0e1ae354458205ccd0e33f41b5ca78c527da33b
Rohit gangurde <rohitgangurde@Rohits-MacBook-Air.local>
2024-12-30 14:04:47 +0530

    Second commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading =React.createElement("h2",{},"Hello1 1335222");
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading);
